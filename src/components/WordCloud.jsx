a5c67deeb72432eb77fe935fa56c0a4e5297c154
Rabin <rabing624@gmail.com>
2026-02-02 19:13:23 +0545

    Dashboard and components

---
+++ src/components/WordCloud.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const WordCloud = () => {
+  return (
+    <div className='px-15 flex flex-col items-start mt-10'>
+        <p className='text-white text-normal font-bold text-2xl'>Word Cloud</p>
+        <div className='bg-[#1B2537] w-full h-auto rounded-lg p-5'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi accusamus ullam, voluptate doloribus odio numquam consequatur laboriosam et sunt quaerat ipsam pariatur. Eveniet mollitia laboriosam ea labore perspiciatis, distinctio maiores consequatur nulla cum veritatis voluptatem temporibus odio inventore officia. Doloremque debitis repellat reprehenderit error vel incidunt aperiam asperiores voluptates voluptatem, vero rerum ullam earum placeat adipisci eveniet, architecto maxime laudantium blanditiis laborum. Laborum eius voluptatum sint dolores sunt necessitatibus molestiae a iure mollitia, dolorum, facere magnam animi exercitationem commodi atque placeat repellat architecto excepturi veritatis? Nostrum tempore accusantium adipisci doloremque veniam nisi sequi eos dolores perferendis? Illo tenetur totam nihil magnam debitis, aperiam placeat eos vero nisi architecto mollitia sint rerum excepturi, repudiandae impedit sunt culpa maxime tempora ipsa quas repellat deleniti quo laborum! Optio nobis, quibusdam et quas deserunt fugit nisi consequuntur, quis accusantium aperiam rem ducimus inventore fugiat accusamus eius earum, sequi perspiciatis quisquam. Officia eaque ipsa vitae minus dignissimos! Blanditiis quidem culpa iste deleniti voluptates recusandae rerum corrupti? Vel similique exercitationem commodi corporis repudiandae consectetur, suscipit laudantium, eius voluptates odio voluptatum adipisci sed modi non sunt praesentium accusantium nulla eos officia voluptatem unde saepe dolore! Eveniet, necessitatibus accusamus explicabo nobis veniam porro maxime fugiat consequatur sapiente hic!
+        </div>
+    </div>
+  )
+}
+
+export default WordCloud
