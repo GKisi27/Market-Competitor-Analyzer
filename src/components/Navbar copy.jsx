d946b672b667f152151bb808b08cf29b92e1612f
Sumit-ghimire <sumitghimire77@gmail.com>
2026-02-17 07:38:39 +0545

    Fixed merge conflicts between Anish and Rabin

---
+++ src/components/Navbar copy.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import logoMca from '../assets/logoMca.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilter, faGear, faUser, faMagnifyingGlass} from '@fortawesome/free-solid-svg-icons'
+
+const Navbar = () => {
+
+    const [searchtext, setSearchtext] = useState('')
+
+    const navigate = useNavigate()
+    const searchHandler =(e)=>{
+        e.preventDefault()
+        setSearchtext('')
+    }
+
+  return (
+    <div className='bg-[#1B2537] h-22.5 flex justify-between items-center w-full px-15 '>
+        <div className='w-18 h-10 '>
+            <img onClick={()=>{
+                console.log("image clicked")
+}} className='h-full w-full' src={logoMca} alt="" />
+        
+        </div>
+        <div className='bg-[#141A28] w-87.5 h-10 p-2 rounded-lg flex justify-between items-center '> 
+            <p className='text-gray-500'>Search</p>
+            <input className='text-white outline-none'
+             onChange={(e)=>{
+                setSearchtext(e.target.value)
+
+             }}
+            value={searchtext} type="text" />
+            <button onClick={searchHandler} > <FontAwesomeIcon className='text-lg text-gray-500 active:scale-110' icon={faMagnifyingGlass} /></button>
+
+        </div>
+
+        <div className='text-white flex justify-between items-center gap-3.5 '>
+            <FontAwesomeIcon className='text-2xl' icon={faFilter} />
+             <FontAwesomeIcon className='text-2xl' icon={faGear} />
+            <FontAwesomeIcon className='text-2xl' icon={faUser} />
+        </div> 
+    </div>
+  )
+}
+
+export default Navbar
---
+++ src/pages/CompetitorsDetails.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CompetitorsDetails = () => {
+  return (
+    <div>CompetitorsDetails</div>
+  )
+}
+
+export default CompetitorsDetails
